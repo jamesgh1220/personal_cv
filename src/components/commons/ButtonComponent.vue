ee95c23ca50922c02b3b468bd17224bc12e21252
James Gallego H <jamesgh.1220@gmail.com>
2025-04-12 15:19:32 -0500

    FEAT: button componebnt

---
+++ src/components/commons/ButtonComponent.vue
@@ -0,0 +1,15 @@
+<template>
+  <button
+    class="flex gap-2 bg-dark text-light font-medium outline outline-sky-300 w-fit px-3 py-2 rounded-lg transition-all duration-200 lg:hover:bg-blue lg:cursor-pointer"
+  >
+    <component :is="icon" v-if="icon" />
+    {{ label }}
+  </button>
+</template>
+
+<script setup>
+defineProps({
+  label: { type: String, required: true },
+  icon: { type: Function, required: true },
+});
+</script>
