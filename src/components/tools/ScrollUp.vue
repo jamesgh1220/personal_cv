d55e91f997af71a991af2da082a04f516d7f28e2
James Gallego H <jamesgh.1220@gmail.com>
2025-04-14 15:36:05 -0500

    FEAT: scroll up component

---
+++ src/components/tools/ScrollUp.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="absolute bottom-4 right-4 z-100">
+    <div class="fadeInUp" v-if="activeClass">
+      <button
+        @click="scrollToTop"
+        round
+        icon="sym_o_arrow_upward"
+        class="h-12 w-12 opacity-80 rounded-full hover:opacity-100 transition-all duration-500 bg-blue"
+      >
+        <ArrowUp class="mx-auto text-light" />
+      </button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ArrowUp } from "lucide-vue-next";
+const activeClass = defineModel("active");
+const scrollContainer = defineModel("container");
+const scrollToTop = () => {
+  if (scrollContainer.value) {
+    scrollContainer.value.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  }
+};
+</script>
+
+<style scoped>
+.height-content {
+  min-height: 0px !important;
+}
+
+.fadeInUp {
+  animation: fadeInUp 0.5s ease forwards;
+}
+
+@keyframes fadeInUp {
+  from {
+    opacity: 0;
+    transform: translateY(70px);
+  }
+  to {
+    opacity: 1;
+    transform: translateY(0);
+  }
+}
+</style>
