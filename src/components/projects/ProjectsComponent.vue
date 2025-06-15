00f8d3ae1cdc3aadde9efc23c6f53fed32700a11
James Gallego H <jamesgh.1220@gmail.com>
2025-06-14 21:21:32 -0500

    CI: init design projects component

--- src/components/projects/ProjectsComponent.vue
+++ src/components/projects/ProjectsComponent.vue
@@ -1,13 +1,27 @@
 <template>
-  <section id="portfolio" class="mb-10 bg-[linear-gradient(to_bottom,_black_0%,_white_5%)]">
+  <section
+    id="portfolio"
+    class="relative mb-10 bg-[linear-gradient(to_bottom,_black_0%,_white_5%)]"
+  >
     <p
+      id="scroll-title"
       v-animate-on-scroll
-      class="relative mt-32 mb-16 w-fit mx-auto text-3xl font-bold before:absolute before:bottom-0 before:left-1/2 before:-translate-x-1/2 before:-mb-4 before:h-0.5 before:w-48 before:bg-black"
+      class="sticky top-16 flex flex-col pt-6 mt-28 w-full text-center text-3xl font-bold bg-white lg:pb-8 lg:mt-20 lg:overflow-hidden"
     >
-      Projectos
+      <span class="lg:hidden">Projectos</span>
+      <span class="line-1 w-full h-1 bg-black inline-block my-2 lg:!hidden"></span>
+      <ul id="scroll-text" class="hidden lg:flex whitespace-nowrap will-change-transform">
+        <li
+          class="flex items-center text-3xl font-bold"
+          v-for="index in 15"
+          :key="index"
+        >
+          <span class="h-2 w-2 bg-black mx-10 rounded-full"></span>
+            {{ 'Proyectos' }}
+        </li>
+      </ul>
     </p>
-    <!-- <title-component title="Proyectos" class="my-20" /> -->
-    <div class="text-light p-4 lg:px-12">
+    <div id="content" class="text-light p-4 lg:px-12">
       <div
         class="grid grid-cols-1 gap-8 sm:grid-cols-2 md:grid-cols-2 min-lg:grid-cols-3 xl:grid-cols-4"
       >
@@ -97,8 +111,51 @@
 
 <script setup>
 import ButtonComponent from "@/components/commons/ButtonComponent.vue";
-import TitleComponent from "@/components/commons/TitleComponent.vue";
 import BadgeComponent from "@/components/commons/BadgeComponent.vue";
 import { Github } from "lucide-vue-next";
 import { ExternalLink } from "lucide-vue-next";
+import { gsap } from "gsap";
+import { ScrollToPlugin } from "gsap/ScrollToPlugin";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { onMounted } from "vue";
+
+gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
+const items = Array.from({ length: 15 });
+
+onMounted(() => {
+  gsap.fromTo(
+    ".line-1",
+    { scaleX: 0 },
+    {
+      scaleX: 1,
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#portfolio",
+        scrub: true,
+        start: "top 90%", // comienza cuando #portfolio entra en la vista
+        end: "bottom 10%", // termina cuando casi sale por arriba
+      },
+    }
+  );
+
+  gsap.to("#scroll-text", {
+    xPercent: -100, // Mueve el texto a la izquierda
+    ease: "none",
+    scrollTrigger: {
+      trigger: "#portfolio",
+      start: "top 90%",
+      end: "bottom 10%",
+      scrub: true,
+    },
+  });
+});
 </script>
+
+<style scoped>
+.line {
+  width: 100%;
+  height: 4px;
+  display: inline-block;
+  background-color: black;
+}
+</style>
